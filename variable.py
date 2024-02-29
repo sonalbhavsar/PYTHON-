a9cb64813809306ad1131f7de94f24c1b35368e3
sonalbhavsar <134159290+sonalbhavsar@users.noreply.github.com>
2024-02-29 21:11:04 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,10 @@
+name = "sonal"
+age = 20
+salary = 150000.76
+print("my name is:", name)
+print("my age is:", age)
+print("my salary is:", salary)
+
+print(type(name))
+print(type(age))
+print(type(salary))
